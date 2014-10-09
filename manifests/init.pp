7041adca5b6327bad59bc6172b10ed73745e7307
Martez Reed <martez.reed@gmail.com>
2014-10-09 08:55:11 -0500

    Cleanup code and fix init.pp error
    Cleanup code and fix init.pp error

--- manifests/init.pp
+++ manifests/init.pp
@@ -17,31 +17,30 @@ class joindomain (
   #$oupath    =  undef,
 
   ) {
- 
+
   # Validate variables
   validate_string($domain)
   validate_string($username)
- 
+
   # Create powershell credential username
   exec { 'Create username':
     command  => "$pscredential_username = $domain\$username",
     provider => powershell,
-  }   
+  }
   # Convert password to powershell secure string
   exec { 'Convert Password':
     command  => "$pscredential_password = ConvertTo-SecureString -String $password -AsPlainText -Force",
     provider => powershell,
   }
   # Create powershell credential object
-  exec { 'Create powershell credential object:'
+  exec { 'Create powershell credential object':
     command  => "$pscredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $pscredential_username, $pscredential_password",
     provider => powershell,
   }
-  # Join the specified domain  
+  # Join the specified domain
   exec { 'Join Domain':
     command  => "Add-Computer -domainname $domain -credential $pscredential",
     provider => powershell,
-
   }
   # Reboot the machine after the domain join operation
   reboot { 'Post Join Reboot':
