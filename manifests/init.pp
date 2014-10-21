a345cc8d5220cc1f6af130379526cf7339d5d923
Martez Reed <martez.reed@gmail.com>
2014-10-21 08:37:30 -0500

    Correct functionality
    The code has been moved into a single exec command to fix the
    functionality.

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,7 +14,7 @@ class joindomain (
   $domain    =  undef,
   $username  =  undef,
   $password  =  undef,
-  #$oupath    =  undef,
+  #$oupath   =  undef,
 
   ) {
 
@@ -22,24 +22,9 @@ class joindomain (
   validate_string($domain)
   validate_string($username)
 
-  # Create powershell credential username
-  exec { 'Create username':
-    command  => "$pscredential_username = $domain\$username",
-    provider => powershell,
-  }
-  # Convert password to powershell secure string
-  exec { 'Convert Password':
-    command  => "$pscredential_password = ConvertTo-SecureString -String $password -AsPlainText -Force",
-    provider => powershell,
-  }
-  # Create powershell credential object
-  exec { 'Create powershell credential object':
-    command  => "$pscredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $pscredential_username, $pscredential_password",
-    provider => powershell,
-  }
-  # Join the specified domain
+  # Join Domain
   exec { 'Join Domain':
-    command  => "Add-Computer -domainname $domain -credential $pscredential",
+    command  => "\$pscredential_password = ConvertTo-SecureString -String ${password} -AsPlainText -Force;\$pscred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username@$domain,\$pscredential_password;Add-Computer -domainname $domain -credential \$pscred",
     provider => powershell,
   }
   # Reboot the machine after the domain join operation
